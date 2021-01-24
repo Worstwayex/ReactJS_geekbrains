8c1348844fd7c2a1ddc9b208f5a3bbdf53078a37
Worstwayex <55409422+Worstwayex@users.noreply.github.com>
2021-01-24 20:48:17 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import './App.css';
-import Messages from "./Components/Messages/MessageField"
+import MessageField from './Components/Messages/MessageField';
 
-class App extends React.Component{
-  state = {
-    messages: [],
-    author: "Mark",
-  }
-  componentDidUpdate(){
-    if(this.state.messages.length %2 === 1)
-    {
-      setTimeout(()=>this.setState({messages: [...this.state.messages,"Ok"]}),1000);
-    }
-  }
-  handleClick = () =>{
-    this.setState(prevState =>({
-      messages: [...prevState.messages,"Normalno"]
-    }))
-  }
-  render(){
-  return (
-    <div className="App">
-        <Messages messages = {this.state.messages} author = {this.state.author}/>
-        <button onClick = {this.handleClick}>Click</button>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <MessageField />
+    );
+  };
 }
 
 export default App;
--- src/Components/Messages/MessageField.js
+++ src/Components/Messages/MessageField.js
@@ -1,9 +1,29 @@
+import React from "react"
 import Message from "./Messages"
-export default function Messages(props){
+export default class MessageField extends React.Component{
     // console.log(props)
+    state = {
+        messages: [],
+        author: "Mark",
+      }
+      componentDidUpdate(){
+        if(this.state.messages.length %2 === 1)
+        {
+          setTimeout(()=>this.setState({messages: [...this.state.messages,"Ok"]}),1000);
+        }
+      }
+      handleClick = () =>{
+        this.setState(prevState =>({
+          messages: [...prevState.messages,"Normalno"]
+        }))
+      }
+    render(){
+
     return(
-        <div>
-            {props.messages.map((message) => <Message message={message} author = {props.author}/>)}
+        <div className="App">
+            {this.state.messages.map((message) => <Message message={message} author = {this.state.author}/>)}
+            <button onClick = {this.handleClick}>Send</button>
         </div>
     )
+    }
 }
